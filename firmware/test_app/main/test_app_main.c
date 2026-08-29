7865e2e3bb117b565504da65054dfedff9bfca1e
Gabriel Nunez <nunezgabrieleduardo@gmail.com>
2026-08-29 18:07:37 -0300

    odenando test_app

--- firmware/test_app/main/test_app_main.c
+++ firmware/test_app/main/test_app_main.c
@@ -14,81 +14,53 @@ void app_main(void)
 
     UNITY_BEGIN();
 
-    /* Ejecuta primero todos los tests con la etiqueta [gpio] */
-    //unity_run_tests_by_tag("[gpio]", false);
+    unity_run_tests_by_tag("[gpio]", false);
 
-    /* Agregas esta línea para ejecutar los de UART automáticamente */
-    //unity_run_tests_by_tag("[uart]", false);
+    unity_run_tests_by_tag("[uart]", false);
 
-    /* Agregas esta línea para ejecutar los de BSP led automáticamente */
-    //unity_run_tests_by_tag("[led]", false);
+    unity_run_tests_by_tag("[led]", false);
 
-    /* Agregas esta línea para ejecutar los de BSP led automáticamente */
-    //unity_run_tests_by_tag("[button]", false);
+    unity_run_tests_by_tag("[button]", false);
 
-    /* Agregas esta línea para ejecutar los de pwrky automáticamente */
-    //unity_run_tests_by_tag("[powerkey]", false);
+    /*BSP-cellular*/
 
+    unity_run_tests_by_tag("[poweron]", false);
 
- 
-    /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[ready]", false);
+    unity_run_tests_by_tag("[at]", false);
 
-      /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[at]", false);
+    unity_run_tests_by_tag("[echo]", false);
 
-          /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[echo]", false);
-
-              /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[full]", false);
+    unity_run_tests_by_tag("[full]", false);
   
-              /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[imsi]", false);
+    unity_run_tests_by_tag("[imsi]", false);
   
-    /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[network]", false);
+    unity_run_tests_by_tag("[network]", false);
 
-    /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[cereg]", false);
+    unity_run_tests_by_tag("[cereg]", false);
 
-    /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[csq]", false);
+    unity_run_tests_by_tag("[csq]", false);
 
-    /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[cops]", false);
+    unity_run_tests_by_tag("[cops]", false);
 
-    /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[pdp]", false);
+    unity_run_tests_by_tag("[pdp]", false);
 
-        /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[act-pdp]", false);
+    unity_run_tests_by_tag("[act-pdp]", false);
 
-           /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[pdp-status]", false);
+    unity_run_tests_by_tag("[pdp-status]", false);
 
-          /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[tcp]", false);
+    unity_run_tests_by_tag("[tcp]", false);
+  
+    unity_run_tests_by_tag("[socket]", false);
+  
+    unity_run_tests_by_tag("[sendrecv]", false);
 
-              /* Agregas esta línea para ejecutar init cellular automáticamente */
+    unity_run_tests_by_tag("[sendrecv_]", false);
+
+    unity_run_tests_by_tag("[send_]", false);
+
+    unity_run_tests_by_tag("[receivetcp_]", false);
+
     unity_run_tests_by_tag("[power-off]", false);
-  
-                 /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[socket]", false);
-  
-
-              /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[sendrecv]", false);
-
-                  /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[sendrecv_]", false);
-
-                  /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[send_]", false);
-
-                  /* Agregas esta línea para ejecutar init cellular automáticamente */
-    //unity_run_tests_by_tag("[receivetcp_]", false);
-
 
     UNITY_END();
 
